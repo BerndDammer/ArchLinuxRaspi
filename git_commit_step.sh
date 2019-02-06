08f82c15a03314394280ad29516272ab2ee26116
maindev <maindev@blackhole.de>
2019-02-06 22:47:20 +0100

    step

---
+++ git_commit_step.sh
@@ -0,0 +1,10 @@
+echo "-------------------------------"
+cat commit_message.txt
+echo "-------------------------------"
+echo "commit message ok ?"
+read
+
+git commit -m "step"
+
+echo "press key"
+read
