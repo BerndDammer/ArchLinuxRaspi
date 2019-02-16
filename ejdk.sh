ade4fd53856a9eec3b6edf3f472e84a5588c329d
maindev <maindev@blackhole.de>
2019-02-16 13:25:05 +0100

    ejdk add final tar building

--- ejdk.sh
+++ ejdk.sh
@@ -6,3 +6,5 @@ tar -xzvpf $EJDK -C ejdk
 JAVA_HOME=/usr/lib/jvm/default-java
 export JAVA_HOME
 ejdk/ejdk1.8.0_201/bin/jrecreate.sh --ejdk-home ejdk/ejdk1.8.0_201 -v --vm all --dest sunjre
+
+tar -czf sunjre.tar.gz sunjre
