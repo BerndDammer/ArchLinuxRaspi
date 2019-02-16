1b908ec0885845c8fcb69f24358bb88ca6d6e161
maindev <maindev@blackhole.de>
2019-02-16 13:11:43 +0100

    step

--- ejdk.sh
+++ ejdk.sh
@@ -4,4 +4,5 @@ mkdir ejdk
 tar -xzvpf $EJDK -C ejdk
 
 JAVA_HOME=/usr/lib/jvm/default-java
+export JAVA_HOME
 ejdk/ejdk1.8.0_201/bin/jrecreate.sh --ejdk-home ejdk/ejdk1.8.0_201 -v --vm all --dest sunjre
