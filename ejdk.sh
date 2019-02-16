7b0e934c10cb3a3f2b77386ae72621622965cb9c
maindev <maindev@blackhole.de>
2019-02-16 12:52:06 +0100

    ejdk name

--- ejdk.sh
+++ ejdk.sh
@@ -2,4 +2,4 @@
 EJDK=ejdk-8u201-linux-armv6-vfp-hflt.tar.gz
 mkdir ejdk
 tar -xzvpf $EJDK -C ejdk
-ejdk/bin/jrecreate.sh --ejdk-home ejdk -v --vm all --dest sunjre
+ejdk/ejdk1.8.0_201/bin/jrecreate.sh --ejdk-home ejdk/ejdk1.8.0_201 -v --vm all --dest sunjre
