0c1ad931e6dacb6812bab75bc92796c69f64767b
maindev <maindev@blackhole.de>
2019-02-17 12:23:44 +0100

    gluon script

--- ejdk.sh
+++ ejdk.sh
@@ -12,3 +12,15 @@ ejdk/ejdk1.8.0_201/bin/jrecreate.sh --ejdk-home ejdk/ejdk1.8.0_201 -v --vm all -
 
 #compress final result
 tar -czf sunjre.tar.gz sunjre
+
+#additionally combine gluon with
+GLUON_ARC=armv6hf-sdk-8.60.9.zip
+GLUON_DIR=armv6hf-sdk
+
+tar -xzvpf $GLUON_ARC
+cp sunjre gluon
+cp {GLUON_DIR}/lib/* gluon/lib  
+cp {GLUON_DIR}/lib/rt/lib/* gluon/lib/
+
+tar -czf gluon.tar.gz gluon
+
