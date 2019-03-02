d36b1bfa7019c5554ba96ef7751649c74640efac
maindev <maindev@blackhole.de>
2019-03-02 13:13:23 +0100

    step

--- write_adds.sh
+++ write_adds.sh
@@ -1,6 +1,16 @@
+echo "******************* define variables"
+LIB11=bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
+LIB9=bellsoft-jre9.0.4-linux-arm32-vfp-hflt.tar.gz
+
+echo "******************* create dirs in opt"
+mkdir sdx2/opt/as
+mkdir sdx2/opt/as/apps
+mkdir sdx2/opt/as
+
+
 
 # create variables
-ALI=ArchLinuxARM-rpi-2-latest.tar.gz
 
-tar -xzvpf $ALI -C sdx2
-mv sdx2/boot/* sdx1
+tar -xzvpf gluon.tar.gz -C sdx2/opt/as
+tar -xzvpf sunjre.tar.gz -C sdx2/opt/as
+tar -xzvpf ${LIB11}/jdk-11.0.2/* sdx2/opt/as/lib11
