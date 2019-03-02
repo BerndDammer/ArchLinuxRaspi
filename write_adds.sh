deb0e549b3b8d1cedcf4913ae8a42430eaba5810
maindev <maindev@blackhole.de>
2019-03-02 13:42:20 +0100

    step

--- write_adds.sh
+++ write_adds.sh
@@ -1,7 +1,10 @@
 echo "******************* define variables"
+
 LIB11=bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
 LIB11V=jdk-11.0.2
+
 LIB9=bellsoft-jre9.0.4-linux-arm32-vfp-hflt.tar.gz
+LIB9V=jre-9.0.4
 
 echo "******************* create dirs in opt"
 mkdir sdx2/opt/as
@@ -14,6 +17,6 @@ mkdir sdx2/opt/as
 
 tar -xzvpf gluon.tar.gz -C sdx2/opt/as
 tar -xzvpf sunjre.tar.gz -C sdx2/opt/as
-tar -xzvpf ${LIB11} sdx2/opt/as
-mv sdx2/opt/as/{}LIB11V sdx2/opt/as/lib11
+tar -xzvpf ${LIB11} -C sdx2/opt/as
+mv sdx2/opt/as/${LIB11V} sdx2/opt/as/lib11
 
